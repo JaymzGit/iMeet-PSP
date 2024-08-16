07aa4048dd0d51632e725a76279a240089565e27
james <jameskhor6@gmail.com>
2024-08-16 20:08:11 +0800

    Implemented OneSignal SDK for notification handling

---
+++ app/src/main/java/com/james/imeetpsp/ApplicationClass.java
@@ -0,0 +1,27 @@
+package com.james.imeetpsp;
+
+import android.app.Application;
+import com.onesignal.OneSignal;
+import com.onesignal.debug.LogLevel;
+import com.onesignal.Continue;
+
+public class ApplicationClass extends Application {
+
+    // NOTE: Replace the below with your own ONESIGNAL_APP_ID
+    private static final String ONESIGNAL_APP_ID = "ade20ce9-1dad-4f0e-b829-5f8bccd19f49";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Verbose Logging set to help debug issues, remove before releasing your app.
+        OneSignal.getDebug().setLogLevel(LogLevel.VERBOSE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this, ONESIGNAL_APP_ID);
+
+        // requestPermission will show the native Android notification permission prompt.
+        // NOTE: It's recommended to use a OneSignal In-App Message to prompt instead.
+        OneSignal.getNotifications().requestPermission(false, Continue.none());
+    }
+}
