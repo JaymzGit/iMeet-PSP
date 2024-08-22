7db83d52d2e20e13e07d3c3762d0b606647ce37c
james <jameskhor6@gmail.com>
2024-08-22 21:47:43 +0800

    Added ForgotPassword activity to send a password reset link to registered email using Firebase

---
+++ app/src/main/java/com/james/imeetpsp/ForgotPassword.java
@@ -0,0 +1,87 @@
+package com.james.imeetpsp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    // Firebase instance
+    FirebaseAuth fAuth;
+
+    // UI elements
+    private EditText etEmail;
+    private Button btnResetPassword;
+    private ImageButton btnBack;
+    private ProgressBar progressBar;
+    String email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        // Apply system-wide dark mode setting
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+
+        // Initialize Firebase instance
+        fAuth = FirebaseAuth.getInstance();
+
+        // Initialize UI elements
+        btnBack = findViewById(R.id.btnBack);
+        etEmail = findViewById(R.id.etEmailAddress);
+        btnResetPassword = findViewById(R.id.btnResetPassword);
+        progressBar = findViewById(R.id.progressBar);
+
+        // Check if a user is already logged in
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            return;
+        }
+
+        // Set up button listeners
+        btnBack.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), StartActivity.class)));
+
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = etEmail.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    etEmail.setError("Email is required.");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                btnResetPassword.setVisibility(View.INVISIBLE);
+
+                fAuth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(ForgotPassword.this, "Reset password link has been sent to your registered email.", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ForgotPassword.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        btnResetPassword.setVisibility(View.VISIBLE);
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        });
+    }
+}
